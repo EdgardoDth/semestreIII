0269988a07609469ca279ee83e18083c49d72270
EdgardoDth <edgardodth@gmail.com>
2017-03-09 23:34:34 -0600

    fibonacci recursiva

---
+++ semi-estructura-de-datos/fibonacci.cpp
@@ -0,0 +1,37 @@
+/*
+ *  SECUENCIA DE FIBONACCI
+ *  PEREYRA CANCIO, EDGARDO
+ * 21 30 55 90 4
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+ int fibonacci(int x)
+ {
+     if(x == 0) {
+        return 0;
+     } else if (x == 1 or x == 2) {
+        return 1;
+     }
+
+     return fibonacci(x - 1) + fibonacci(x - 2);
+ }
+
+ int main()
+ {
+    int a, c = 0;
+
+    do {
+        cout << "Ingrese el numero del cual desea su secuencia de fibonacci: ";
+        cin >> a;
+
+        cout << "El resultado es: " << fibonacci(a) << endl << endl;
+        cout << "Si desea hacer otra operacion ingrese el numero 0" << endl;
+        cout << ", sino ingrese cualquier numero: ";
+        cin >> c;
+    } while(c == 0);
+
+    return 0;
+ }
