9dff9aa3a1935bd2f527c48e04cc654022dd77cc
EdgardoDth <edgardodth@gmail.com>
2017-03-31 23:11:44 -0600

    eliminar funcion creada para usar la funcion que incluye la biblioteca math

--- estructura-de-datos-II/dis-poisson.py
+++ estructura-de-datos-II/dis-poisson.py
@@ -1,19 +1,9 @@
-# programa que calcula la distribuicion de poisson
+#programa que calcula la distribuicion de poisson
 
 import math
 
-def factorial(x):
-    if x == 0:
-        return 1
-    else:
-        return x * factorial(x - 1)
-
-# donde R es el numero de registros almacenados en un archivo
-# donde N es el numero de direcciones posibles en un archivo
-# donde X es el numero de registros asociados en un archivo
-
 def disPoisson(r, n, x):
-    num = (((r / n) ** x) * math.exp(-(r / n))) / factorial(x)
+    num = (((r / n) ** x) * math.exp(-(r / n))) / math.factorial(x)
     return num
 
 numN = (int)(input("Ingrese el numero de direcciones posibles: "))
